2b279743ade9e8a4dedb5dd527e46aef24c5972f
Tutu12344 <padde.cross@gmail.com>
2023-03-02 21:01:25 +0900

    button test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        type="button"
+        style={{
+          width: "100%",
+          textAlign: "center",
+          backgroundColor: "#ff8c00",
+          color: "#f1f1f1",
+          padding: "0.5rem",
+        }}
+      ></button>
     </div>
   );
 }
